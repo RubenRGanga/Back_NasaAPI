cbe927e57da90707ead2f187ce183047f5c00311
RubenRGanga <rubenruizganga@gmail.com>
2022-12-23 09:34:51 +0100

    Añadido Cors

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -7,10 +7,13 @@ const landings = require('../routes/landings');
 const neas = require('../routes/neas');
 const users = require('../routes/users');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 module.exports = function (app) {
+
+    app.use(cors())
     app.use(express.json())
     
     app.use('/api/astronomy/landings', landings) 
